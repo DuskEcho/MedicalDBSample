53349a7ff673ad20d3d4ef7909c8c937ed7ea07d
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2019-10-05 11:57:26 -0700

    Create proceduresLookup.sql

---
+++ queries/proceduresLookup.sql
@@ -0,0 +1,13 @@
+# The following query is used to display a/many procedure(s) for a patient
+# Patient name, description, date, and physician are shown as part 
+# of the summary
+#
+#  In the servlet, the last two line is in form:
+# "WHERE FirstName LIKE ? AND LastName LIKE ? AND ProcedureID LIKE ?"
+# Actual values are given here for quick-testing
+
+SELECT FirstName, LastName, ProcedureDescription, ProcedureDate, ph.PhysicianName as PhysicianName 
+FROM patient 
+NATURAL JOIN procedures p 
+JOIN physicians ph  ON p.PhysicianID = ph.PhysicianID
+WHERE FirstName LIKE "Frank" AND LastName LIKE "Redbeard" AND ProcedureID LIKE "2"
