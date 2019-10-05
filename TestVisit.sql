e835168a05a7960a0b6de86b9970d54812f9b4da
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2019-10-04 19:49:51 -0700

    added physician ids to test cases

--- TestVisit.sql
+++ TestVisit.sql
@@ -1,11 +1,11 @@
-INSERT INTO visit (VisitDescription, VisitDate, Completed, PatientID)
-VALUES ("Leg complaint, eye complaint", "2010-09-01", true, 1),
-	("chest pain", "2010-08-26", true, 2),
-    ("soreness in arm", "2010-08-29", false, 3),
-    ("pain when walking", "2010-09-02", true, 4),
-    ("difficulty sleeping", "2010-09-01", true, 5),
-    ("Difficulty breathing, difficulty walking", "2010-07-16", true, 6),
-    ("Follow up to visit from 2010-07-16", "2010-07-23", false, 6),
-    ("Discoloration in veins", "2010-08-29", true, 7),
-    ("'fingers smell like chicken'", "2010-09-04", true, 9)
+INSERT INTO visit (VisitDescription, VisitDate, Completed, PatientID, PhysicianID)
+VALUES ("Leg complaint, eye complaint", "2010-09-01", true, 1, 2),
+	("chest pain", "2010-08-26", true, 2, 1),
+    ("soreness in arm", "2010-08-29", false, 3, 3),
+    ("pain when walking", "2010-09-02", true, 4, 3),
+    ("difficulty sleeping", "2010-09-01", true, 5, 3),
+    ("Difficulty breathing, difficulty walking", "2010-07-16", true, 6, 1),
+    ("Follow up to visit from 2010-07-16", "2010-07-23", false, 6, 1),
+    ("Discoloration in veins", "2010-08-29", true, 7, 3),
+    ("'fingers smell like chicken'", "2010-09-04", true, 9, 2)
     
