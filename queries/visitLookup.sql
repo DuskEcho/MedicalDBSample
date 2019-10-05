02ff1357e3476d1fe16cb2b061906f09f80030a9
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2019-10-05 11:52:53 -0700

    Create visitLookup.sql

---
+++ queries/visitLookup.sql
@@ -0,0 +1,16 @@
+# The following query is used to look up a patient's visits by most "recent".
+# In the case of appointments, appointments are displayed from
+# furthest out to closest. 
+#
+#  In the servlet, the last two lines are in form:
+# "WHERE FirstName LIKE ? AND LastName LIKE ? AND
+# v.Completed = ? ORDER BY VisitDate DESC;"
+# Actual values are given here for quick-testing
+
+
+SELECT FirstName, LastName, ph.PhysicianName as PhysicianName, VisitDescription, VisitDate
+FROM Patient p
+JOIN visit v ON p.PatientId = v.PatientId
+JOIN physicians ph on ph.`PhysicianID` = v.`PhysicianID`
+WHERE FirstName LIKE ? AND LastName LIKE ? AND
+v.Completed = ? ORDER BY VisitDate DESC;
