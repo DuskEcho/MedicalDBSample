bdbdcfb0cf27cfb1ec841c7f2863867d829be832
Dalia <43527456+dollybuzz@users.noreply.github.com>
2019-10-14 17:21:52 -1000

    Updated SchemaSetup Conditions insert table values

--- SchemaSetup.sql
+++ SchemaSetup.sql
@@ -153,25 +153,25 @@ INSERT INTO physicians
 VALUES (3, "third", "thirdnum");
 
 INSERT INTO patient
-VALUES (1, "Frank", "Redbeard", 1950-01-01, "111-111-1111", "", "", 185.0, 5.9, "O");
+VALUES (1, "Frank", "Redbeard", 1950-01-01, "111-111-1111", "", "", 185.0, 59, "O");
 INSERT INTO patient
-VALUES (2, "Jack", "Doe", 1935-06-15, "222-222-2222", "Matka", "Pluku", 149.0, 5.8, "AB-");
+VALUES (2, "Jack", "Doe", 1935-06-15, "222-222-2222", "Matka", "Pluku", 149.0, 58, "AB-");
 INSERT INTO patient
-VALUES (3, "Matka", "Pluku", 1936-05-15, "333-333-3333", "", "", 185.0, 5.9, "O-");
+VALUES (3, "Matka", "Pluku", 1936-05-15, "333-333-3333", "", "", 185.0, 59, "O-");
 INSERT INTO patient
-VALUES (4, "Karina", "Nose", 1984-02-21, "444-444-4444", "Hiro", "Nose", 105.6, 5.1, "A+");
+VALUES (4, "Karina", "Nose", 1984-02-21, "444-444-4444", "Hiro", "Nose", 105.6, 51, "A+");
 INSERT INTO patient
-VALUES (5, "Hasan", "Minhaj", 1985-09-23, "555-555-5555", "Beena", "Patel", 162.4, 6, "B+");
+VALUES (5, "Hasan", "Minhaj", 1985-09-23, "555-555-5555", "Beena", "Patel", 162.4, 60, "B+");
 INSERT INTO patient
-VALUES (6, "Bruce", "Wayne", 1955-02-19, "666-666-6666", "Selina", "Kyle", 210.0, 6.15, "AB+");
+VALUES (6, "Bruce", "Wayne", 1955-02-19, "666-666-6666", "Selina", "Kyle", 210.0, 61, "AB+");
 INSERT INTO patient
-VALUES (7, "Pamela", "Isley", 1966-06-01, "777-777-7777", "Cary", "Bates", 185.0, 5.7, "B-");
+VALUES (7, "Pamela", "Isley", 1966-06-01, "777-777-7777", "Cary", "Bates", 185.0, 57, "B-");
 INSERT INTO patient
-VALUES (8, "Greta", "Thunberg", 2003-01-03, "888-888-8888", "Olaf", "Thunberg", 120.0, 5.5, "O+");
+VALUES (8, "Greta", "Thunberg", 2003-01-03, "888-888-8888", "Olaf", "Thunberg", 120.0, 55, "O+");
 INSERT INTO patient
-VALUES (9, "Philip", "Fry", 1974-08-14, "999-999-9999", "Turanga", "Leela", 165.0, 5.75, "A+");
+VALUES (9, "Philip", "Fry", 1974-08-14, "999-999-9999", "Turanga", "Leela", 165.0, 57, "A+");
 INSERT INTO patient
-VALUES (10, "John", "Bercow", 1963-01-19, "111-222-3333", "Sally", "Bercow", 160.0, 5.5, "O-");
+VALUES (10, "John", "Bercow", 1963-01-19, "111-222-3333", "Sally", "Bercow", 160.0, 55, "O-");
 
 INSERT INTO visit (VisitDescription, VisitDate, Completed, PatientID, PhysicianID)
 VALUES ("Leg complaint, eye complaint", "2010-09-01", true, 1, 2),
