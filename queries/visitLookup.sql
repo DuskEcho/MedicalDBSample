6af768ea81cf4bfab8efdcca7885c45319f51e6b
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2019-10-17 19:44:02 -0700

    Update visitLookup.sql

--- queries/visitLookup.sql
+++ queries/visitLookup.sql
@@ -1,11 +1,11 @@
-# The following query is used to look up a patient's visits by most "recent".
-# In the case of appointments, appointments are displayed from
-# furthest out to closest. 
-#
-#  In the servlet, the last two lines are in form:
-# "WHERE FirstName LIKE ? AND LastName LIKE ? AND
-# v.Completed = ? ORDER BY VisitDate DESC;"
-# Actual values are given here for quick-testing
+-- The following query is used to look up a patient's visits by most "recent".
+-- In the case of appointments, appointments are displayed from
+-- furthest out to closest. 
+--
+--  In the servlet, the last two lines are in form:
+-- "WHERE FirstName LIKE ? AND LastName LIKE ? AND
+-- v.Completed = ? ORDER BY VisitDate DESC;"
+-- Actual values are given here for quick-testing
 
 
 SELECT FirstName, LastName, ph.PhysicianName as PhysicianName, VisitDescription, VisitDate
