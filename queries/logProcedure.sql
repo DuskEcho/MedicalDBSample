ab588417616c857dddb893c9529e3bb57acad37c
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2019-10-17 19:45:14 -0700

    Update logProcedure.sql

--- queries/logProcedure.sql
+++ queries/logProcedure.sql
@@ -1,12 +1,12 @@
-# The following query is used to log a procedure performed
-# Note that it is the user's responsibility to enter valid Condition and PatientIDs.
-# This is assumed to be performed in an environment where both are readily available, e.g.,
-# At the clinic just after a procedure with PatientID and ConditionID physically present in some form
-# (ID card, condition info "pulled up" on the monitor)
-#
-#  In the servlet, the last line is in form:
-# "(SELECT PatientID FROM patient WHERE FirstName = ? AND LastName = ?), ?, ?, ?, ?, ?"
-# Actual values are given here for quick-testing
+-- The following query is used to log a procedure performed
+-- Note that it is the user's responsibility to enter valid Condition and PatientIDs.
+-- This is assumed to be performed in an environment where both are readily available, e.g.,
+-- At the clinic just after a procedure with PatientID and ConditionID physically present in some form
+-- (ID card, condition info "pulled up" on the monitor)
+--
+--  In the servlet, the last line is in form:
+-- "(SELECT PatientID FROM patient WHERE FirstName = ? AND LastName = ?), ?, ?, ?, ?, ?"
+-- Actual values are given here for quick-testing
 
 INSERT INTO procedures (PatientID, ProcedureDescription, ProcedureDate, ConditionID, Result, PhysicianID)
 VALUES (
