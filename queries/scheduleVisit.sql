b0102490b4740f8dafb4025e1c39948ebff79988
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2019-10-17 19:44:28 -0700

    Update scheduleVisit.sql

--- queries/scheduleVisit.sql
+++ queries/scheduleVisit.sql
@@ -1,11 +1,11 @@
-# The following query is used to schedule a visit for a patient
-# Note that "false" is always used for column "completed"
-# as it should not be possible to "schedule" a visit that 
-# has already been completed.
-#
-#  In the servlet, the last two line is in form:
-# "(SELECT PatientID FROM patient WHERE FirstName = ? AND LastName = ?), ?, ?, ?, false);"
-# Actual values are given here for quick-testing
+-- The following query is used to schedule a visit for a patient
+-- Note that "false" is always used for column "completed"
+-- as it should not be possible to "schedule" a visit that 
+-- has already been completed.
+--
+--  In the servlet, the last two line is in form:
+-- "(SELECT PatientID FROM patient WHERE FirstName = ? AND LastName = ?), ?, ?, ?, false);"
+-- Actual values are given here for quick-testing
 
 INSERT INTO visit (PatientID, VisitDescription, VisitDate, PhysicianID, Completed) VALUES (
 (SELECT PatientID FROM patient WHERE FirstName = "Frank" AND LastName = "Redbeard"), "wobbly peg", "2019-10-05", 1, false);
